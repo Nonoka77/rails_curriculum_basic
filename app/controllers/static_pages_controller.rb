e859c332eaea06000c5455f1bee22a966031af04
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-08-25 14:53:08 +0900

    Finish Sorcery Install

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+    skip_before_action :require_login
     def top
         render "/static_pages/top"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "static_pages#top"
   get 'static_pages/top', to: 'static_pages#top'
+
+  resources :users, only: [:new, :create]
+  get '/login', to: 'user_sessions#new', as: :login
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy', as: :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
