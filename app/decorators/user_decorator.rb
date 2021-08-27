9abaeb005b80a4672ff6091f98463a449a139279
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-08-27 13:48:09 +0900

    Set Decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,13 +1,7 @@
 class UserDecorator < ApplicationDecorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
+  def full_name
+    "#{model.last_name} #{model.first_name}"
+  end
 end
