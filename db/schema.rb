bf238a81896c54650aad4c94e127b667de4f830f
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-08-27 15:43:16 +0900

    Create Board

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_025331) do
+ActiveRecord::Schema.define(version: 2021_08_27_063855) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
