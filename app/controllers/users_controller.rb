17f4fd748a5a3d64185bf4c4def3ba8629089f2c
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-08-27 09:45:13 +0900

    Set Ja.yml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create]
+  skip_before_action :require_login, only: %i[new create]
   def new
     @user = User.new
   end
@@ -7,15 +7,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save!
-      redirect_to login_path, success: 'ユーザー登録が完了しました'
+      redirect_to login_path, success: t('.success')
     else
-      flash.now[:danger] = 'ユーザー登録に失敗しました'
+      flash.now[:danger] = t('.fail')
       render :new
      end
   end
 
   private
-   def user_params
-     params.require(:user).permit(:email, :password, :password_confirmation, :last_name, :first_name)
-   end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :last_name, :first_name)
+  end
 end
