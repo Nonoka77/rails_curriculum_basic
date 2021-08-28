aabe24c57925fee024fdb02b655f0e478183f12c
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-08-28 09:42:47 +0900

    Set Faker To Seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 20.times do
-    User.create! (
-        first_name: Faker::Name.first_name
-        last_name: Faker::Name.last_name
-        email: Faker::Internet.email
-        password: 'foobar'
+    User.create! ({
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        password: 'foobar',
         password_confirmation: 'foobar'
-    )
+    })
 end
 
 20.times do |n|
-    Board.create! (
-        title: "タイトル #{n + 1}"
-        body: " 本文　#{n +1}"
-    )
+    Board.create! ({
+        title: "タイトル #{n + 1}",
+        body: " 本文　#{n + 1}",
+        user: User.offset(rand(User.count)).first
+    })
 end
