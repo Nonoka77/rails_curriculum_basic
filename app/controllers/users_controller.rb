5b4bb8564f6b795559a25a27f8e57f99e6b95b57
Nonoka77 <84692432+Nonoka77@users.noreply.github.com>
2021-08-29 08:58:45 +0900

    Finish Add Error Messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       redirect_to login_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
